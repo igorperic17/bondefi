961c677bc7cb816567909987dc57b2b7dcb646f9
Branko <saicoder.net@gmail.com>
2024-09-25 18:25:05 +0200

    token updates

--- webapp/src/lib/radix/manifest/create-token.ts
+++ webapp/src/lib/radix/manifest/create-token.ts
@@ -1,55 +1,48 @@
-import { string } from '../utils'
+import { dateToUnixTimestamp, string } from '../utils'
 
 export interface CreateTokenProps {
-  collateralAddress: string
-  name: string
-  symbol: string
-  description: string
-  iconUrl: string
-  projectUrl: string
-  launchDate: string
-  tagList: [],
-  saleEndDate: string
-  fundingTarget: number
+	collateralAddress: string
+	name: string
+	symbol: string
+	description: string
+	iconUrl: string
+	projectUrl: string
+	tagList: []
 
-  bondingCurveType: number
-  bondingCurveParameters: number[]
-}
+	saleStart: Date
+	saleEnd: Date
+	presaleGoal: string
 
-export const EXAMPLE_CREATE_TOKEN_PROPS = {
-  collateralAddress:
-    'resource_tdx_2_1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxtfd2jc', // RDX on Stokenet
-  name: 'BonDeFi',
-  symbol: 'BDFI',
-  description: 'BonDeFi Token',
-  iconUrl: 'https://bondefi.com/icon.png',
-  launchDate: '2023-07-01',
-  saleEndDate: '2023-08-01',
-  fundingTarget: 100000,
+	bondingCurveType: number
+	bondingCurveParameters: number[]
 }
 
 export const createTokenManifest = (
-  accountId: string,
-  componentId: string,
-  props: CreateTokenProps
+	accountId: string,
+	componentId: string,
+	props: CreateTokenProps,
 ) => {
-  const curveParams = props.bondingCurveParameters
-    .map((t) => `Decimal(${string(t.toString())})`)
-    .join(', ')
+	const curveParams = props.bondingCurveParameters
+		.map((t) => `Decimal(${string(t.toString())})`)
+		.join(', ')
 
-  return `
+	const tags = props.tagList.map(string).join(', ')
+
+	return `
         CALL_METHOD
             Address("${componentId}")
             "create_token"
             Address(${string(props.collateralAddress)})
             Enum<${props.bondingCurveType}u8>(${curveParams})
+            ${dateToUnixTimestamp(props.saleStart)}i64 
+            ${dateToUnixTimestamp(props.saleEnd)}i64
+            Decimal(${string(props.presaleGoal)})
             ${string(props.name)}
             ${string(props.symbol)}
             ${string(props.description)}
+            Array<String>(${tags})
             ${string(props.iconUrl)}
-            ${string(props.launchDate)}
-            ${string(props.saleEndDate)}
-            Decimal("${props.fundingTarget}")
+            ${string(props.projectUrl)}
         ;
 
         CALL_METHOD
