3a8628f6098ca67c638e7c7f86ee4355fc18060b
Igor Peric <igorperic@live.com>
2024-09-25 13:09:43 +0200

    Initial layout

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import AnimatedGridPattern from "@/components/magicui/animated-grid-pattern";
+import { cn } from "@/lib/utils";
+import Header from "../components/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +15,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className="dark">
+      <body>
+        {/* <AnimatedGridPattern
+          numSquares={50}
+          maxOpacity={0.4}
+          duration={1}
+          repeatDelay={1}
+          className={cn(
+            "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+            "fixed inset-0 z-0 opacity-80 inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+          )}
+        /> */}
+        <div className="flex flex-col min-h-screen relative z-10">
+          <Header className="w-full max-w-screen" />
+          <div className="flex flex-1 pt-16">
+            {/* <div className="h-screen">
+              <Sidebar />
+            </div> */}
+            <main className="flex-1 p-32">{children}</main>
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ webapp/src/components/header.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import React from 'react';
+import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit';
+import { cn } from '@/lib/utils'; // Assuming you have a utility function for class merging
+import { useRouter } from 'next/navigation';
+import { CoinsIcon, HomeIcon, LayoutDashboardIcon, RocketIcon } from 'lucide-react';
+import { motion } from 'framer-motion';
+import { RadixConnectButton } from '@/lib/radix';
+
+interface HeaderProps {
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ className }) => {
+    const router = useRouter();
+
+    return (
+        <div className={cn("fixed top-0 left-0 right-0 w-full z-10 bg-black bg-opacity-50 backdrop-blur-md", className)}>
+            <div className="max-w-screen mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between items-center py-4">
+                    <div className="flex items-center">
+                        <h1 className="text-xl font-bold text-white">bondefi.xyz</h1>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="flex space-x-4">
+                            <motion.div
+                                onClick={() => router.push('/')}
+                                whileHover={{ scale: 1.1 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                title="Home"
+                                className="flex flex-col items-center"
+                            >
+                                <HomeIcon className="cursor-pointer" />
+                                <span className="text-xs text-white">Home</span>
+                            </motion.div>
+                            <motion.div
+                                onClick={() => router.push('/token')}
+                                whileHover={{ scale: 1.1 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                title="Tokens"
+                                className="flex flex-col items-center"
+                            >
+                                <CoinsIcon className="cursor-pointer" />
+                                <span className="text-xs text-white">Tokens</span>
+                            </motion.div>
+                            <motion.div
+                                onClick={() => router.push('/#')}
+                                whileHover={{ scale: 1.1 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                title="Dashboard"
+                                className="flex flex-col items-center"
+                            >
+                                <LayoutDashboardIcon className="cursor-pointer" />
+                                <span className="text-xs text-white">Dashboard</span>
+                            </motion.div>
+                            <motion.div
+                                onClick={() => router.push('/token/launch')}
+                                whileHover={{ scale: 1.1 }}
+                                transition={{ type: "spring", stiffness: 300 }}
+                                title="Launch Token"
+                                className="flex flex-col items-center"
+                            >
+                                <RocketIcon className="cursor-pointer" />
+                                <span className="text-xs text-white">Launch</span>
+                            </motion.div>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        {/* <RadixConnectButton /> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
