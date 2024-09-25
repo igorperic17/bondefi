412d13cfd5ab04d1fe788e6e10b4491a05f89214
Bruno Fondevila Rodríguez <bruno@mobon.io>
2024-09-25 23:57:09 +0200

    Feature/bancor (#1)
    * Bonding curve implementation
    
    * Fixing bonding curve buy calculation
    
    * Passing test for bancor buy
    
    * Adding back dependencies that are causing issues, but commented out

--- contract/src/bonding_curve.rs
+++ contract/src/bonding_curve.rs
@@ -3,10 +3,7 @@ use scrypto_math::*;
 
 #[derive(ScryptoSbor, Debug, Clone, ManifestSbor)]
 pub enum BondingCurve {
-    Bancor {
-        reserve_ratio: Decimal,
-        starting_price: Decimal,
-    },
+    Bancor { reserve_ratio: Decimal },
 }
 
 impl BondingCurve {
@@ -17,25 +14,12 @@ impl BondingCurve {
         amount_paid: Decimal,
     ) -> Decimal {
         match self {
-            //buyAmt = tokenSupply * ((1 + amtPaid / collateral)^CW — 1)
-            BondingCurve::Bancor {
-                reserve_ratio,
-                starting_price,
-            } => {
-                // let mut start: Decimal = Decimal::from(0);
-                // if collateral_reserves == dec!(0) {
-                //     start = *starting_price;
-                // }
-                // token_reserves
-                //     * (((Decimal::ONE + (amount_paid / collateral_reserves)).pow(*reserve_ratio))
-                //         .unwrap()
-                //         - Decimal::ONE)
-                let new_collateral: Decimal = collateral_reserves + amount_paid;
-                let new_total_supply: Decimal = new_collateral / *reserve_ratio;
-                token_reserves
-                    * (((Decimal::ONE + (amount_paid / collateral_reserves)).pow(*reserve_ratio))
+            //buyAmt = tokenSupply * ((1 + amtPaid / collateral)^CW — 1)s
+            BondingCurve::Bancor { reserve_ratio } => {
+                (token_reserves + Decimal::ONE)
+                    * (Decimal::ONE + amount_paid / (collateral_reserves + *reserve_ratio))
+                        .pow(*reserve_ratio)
                         .unwrap()
-                        - Decimal::ONE)
             }
         }
     }
@@ -55,14 +39,9 @@ impl BondingCurve {
 impl std::fmt::Display for BondingCurve {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            BondingCurve::Bancor {
-                reserve_ratio,
-                starting_price,
-            } => write!(
-                f,
-                "reserve_ratio:{}, starting_price:{}",
-                reserve_ratio, starting_price
-            ),
+            BondingCurve::Bancor { reserve_ratio } => {
+                write!(f, "reserve_ratio:{}", reserve_ratio)
+            }
         }
     }
 }
