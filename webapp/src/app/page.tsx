f88e8d0ca98636718102ddf858361ca4bf2e281a
Igor Peric <igorperic@live.com>
2024-09-25 15:12:30 +0200

    Added main page sections

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import Features from "@/components/bento";
+import BondingSection from "@/components/bonding";
+import FooterSection from "@/components/footer";
+import RadixSection from "@/components/radix";
+import RoadmapSection from "@/components/roadmap";
+import TeamSection from "@/components/team";
+import { TextRevealSection } from "@/components/text-reveal";
 import Hero from "@components/header";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      {/* <TokenSlider tokens={mockTokens} /> */}
+      <TextRevealSection text="Imagine launching an instantly tradable token with built-in guaranteed liquitidy... forever." />
+      <BondingSection />
+      <Features />
+      <RadixSection />
+      <RoadmapSection />
+      <TeamSection />
+      <FooterSection />
     </>
   );
 }
